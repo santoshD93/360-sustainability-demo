dce785e76ccb61fca89b37301bc3135d5deee66e
santoshdhirwani <santosh@mainteny.com>
2025-08-06 14:08:42 +0200

    feat: init supabase client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Next: <Route path="/brands" element={<BrandList />} /> */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* Add more routes here */}
+    </Routes>
   );
 }
 
